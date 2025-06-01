d15365fdb4772665a263c57905146b820f44e81d
Ivan Dutra Goetze <ivangoetze@gmail.com>
2025-06-01 18:27:22 -0300

    Atualizar o HojaSprites.java

--- src/graficos/HojaSprites.java
+++ src/graficos/HojaSprites.java
@@ -2,36 +2,48 @@ package graficos;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 public class HojaSprites {
-	private final int ancho;
-	private final int alto;
-	public final int[] pixeles;
-	
-	//coleccion de hojas de sprites 
-	public static HojaSprites desierto = new HojaSprites("/texturas/HojaSprites.png", 416, 416); // Deve ser indicada a imagem e tamanho da imagem
-	public static HojaSprites jogador = new HojaSprites("/imagenes/hojasPersonajes/2.png", 128, 96); // Deve ser indicada a imagem e tamanho da imagem
-	
-	//fim de la coleccion
-	
-	public HojaSprites(final String ruta, final int ancho, final int alto) {
-		this.ancho = ancho;
-		this.alto  = alto;
-		
-		pixeles = new int[ancho * alto];
-		
-		try {
-			BufferedImage image = ImageIO.read(HojaSprites.class.getResource(ruta));
-			
-			image.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public int obtenAncho() {
-		return ancho;
-	}
+
+    private final int ancho;
+    private final int alto;
+    public final int[] pixeles;
+
+    public static HojaSprites ladrilho = new HojaSprites("/texturas/ladrilho.png");
+    public static HojaSprites jogador = new HojaSprites("/texturas/jogador.png");
+    public static HojaSprites casa = new HojaSprites("/texturas/casa.png");
+    public static HojaSprites paredes = new HojaSprites("/texturas/paredes.png");
+    public static HojaSprites muro = new HojaSprites("/texturas/muro.png");
+    public static HojaSprites seguranca = new HojaSprites("/texturas/seguranca.png");
+    public static HojaSprites casaMadeira = new HojaSprites("/texturas/casa_madeira.png");
+    public static HojaSprites casaCimento = new HojaSprites("/texturas/casa_cimento.png");
+
+    public HojaSprites(final String ruta) {
+        BufferedImage imagen = null;
+
+        try {
+            imagen = ImageIO.read(HojaSprites.class.getResourceAsStream(ruta));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.ancho = imagen.getWidth();
+        this.alto = imagen.getHeight();
+
+        this.pixeles = new int[ancho * alto];
+        imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public int getAlto() {
+        return alto;
+    }
+
+    public int[] getPixeles() {
+        return pixeles;
+    }
 }
