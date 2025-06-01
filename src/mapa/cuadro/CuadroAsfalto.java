c024f1ef834b66fc69f883469fdc312e30470569
Ivan Dutra Goetze <ivangoetze@gmail.com>
2025-06-01 18:19:29 -0300

    Atualizar o CuadroAsfalto.java

--- src/mapa/cuadro/CuadroAsfalto.java
+++ src/mapa/cuadro/CuadroAsfalto.java
@@ -3,7 +3,8 @@ package mapa.cuadro;
 import graficos.Sprite;
 
 public class CuadroAsfalto extends Cuadro {
-	public CuadroAsfalto(Sprite sprite) {
-		super(sprite);
-	}	
+
+    public CuadroAsfalto(Sprite sprite) {
+        super(sprite, false); // Asfalto geralmente não é sólido
+    }
 }
