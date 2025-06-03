8dd3113dac9890f50739bbab264aa7d0158307a4
Ivan Dutra Goetze <ivangoetze@gmail.com>
2025-06-03 15:54:13 -0300

    Update Criatura.java

--- src/entes/criaturas/Criatura.java
+++ src/entes/criaturas/Criatura.java
@@ -49,49 +49,52 @@ public abstract class Criatura extends Ente {
 	}
 	
 	private boolean enColision(int desplazamientoX, int desplazamientoY) {
-		boolean colision = false; // Removidos marcadores [span_X]
+    boolean colision = false;
 
-		int proximaPosicionX = x + desplazamientoX;
-		int proximaPosicionY = y + desplazamientoY;
+    int proximaPosicionX = x + desplazamientoX;
+    int proximaPosicionY = y + desplazamientoY;
 
-		// Margens de colisão dentro do sprite da criatura
-		// AJUSTE ESTES VALORES SE SEUS SPRITES TIVEREM MARGENS DIFERENTES
-		int margenIzquierda = 6; 
-		int margenDerecho = 26; 
-		int margenSuperior = 16; 
-		int margenInferior = 30; 
+    // Margens da caixa de colisão (ajuste conforme o design do seu sprite)
+    int margenXInicial = 8; // ex: 8 pixels da esquerda do sprite
+    int margenXFinal = 24; // ex: 24 pixels da esquerda do sprite (largura da caixa: 24-8 = 16px)
+    int margenYInicial = 16; // ex: 16 pixels do topo do sprite
+    int margenYFinal = 30; // ex: 30 pixels do topo do sprite (altura da caixa: 30-16 = 14px)
 
-		int pixelEsquerda = proximaPosicionX + margenIzquierda;
-		int pixelDireita = proximaPosicionX + margenDerecho;
-		int pixelCima = proximaPosicionY + margenSuperior;
-		int pixelBaixo = proximaPosicionY + margenInferior;
+    // Coordenadas em pixels dos 4 cantos da *próxima* caixa de colisão
+    int pixelEsquerdaSuperiorX = proximaPosicionX + margenXInicial;
+    int pixelEsquerdaSuperiorY = proximaPosicionY + margenYInicial;
 
-		// Converter as coordenadas de pixel para coordenadas de tile
-		// Cuadro.LADO é o tamanho do tile (ex: 32 pixels)
-		// Alterado de sprite.obtemLado() para sprite.getLado()
-		int tileEsquerda = pixelEsquerda / sprite.getLado(); 
-		int tileDireita = pixelDireita / sprite.getLado(); 
-		int tileCima = pixelCima / sprite.getLado(); 
-		int tileBaixo = pixelBaixo / sprite.getLado(); 
+    int pixelDireitaSuperiorX = proximaPosicionX + margenXFinal;
+    int pixelDireitaSuperiorY = proximaPosicionY + margenYInicial;
 
-		// Verificar a solidez dos 4 cantos da caixa de colisão
-		// Assumindo que mapa.obtenerCuadro(x, y) retorna um Cuadro
-		// e que Cuadro tem um método esSolido()
-		if (mapa.obtenerCuadro(tileEsquerda, tileCima).esSolido()) { // Removidos marcadores [span_X]
-			colision = true;
-		}
-		if (mapa.obtenerCuadro(tileDireita, tileCima).esSolido()) { // Removidos marcadores [span_X]
-			colision = true;
-		}
-		if (mapa.obtenerCuadro(tileEsquerda, tileBaixo).esSolido()) { // Removidos marcadores [span_X]
-			colision = true;
-		}
-		if (mapa.obtenerCuadro(tileDireita, tileBaixo).esSolido()) { // Removidos marcadores [span_X]
-			colision = true;
-		}
+    int pixelEsquerdaInferiorX = proximaPosicionX + margenXInicial;
+    int pixelEsquerdaInferiorY = proximaPosicionY + margenYFinal;
 
-		return colision;
-	}
+    int pixelDireitaInferiorX = proximaPosicionX + margenXFinal;
+    int pixelDireitaInferiorY = proximaPosicionY + margenYFinal;
+
+    // Converter para coordenadas de tile
+    // Usar Cuadro.LADO para dividir, se for o lado do tile, ou sprite.getLado() se o sprite é o tile
+    // Assumindo que sprite.getLado() é o tamanho do tile (32 pixels)
+    int ladoTile = sprite.getLado(); // Ou Cuadro.LADO se for constante em Cuadro
+
+    // Verificar os 4 pontos de colisão
+    if (mapa.obtenerCuadro(pixelEsquerdaSuperiorX / ladoTile, pixelEsquerdaSuperiorY / ladoTile).esSolido()) {
+        colision = true;
+    }
+    if (mapa.obtenerCuadro(pixelDireitaSuperiorX / ladoTile, pixelDireitaSuperiorY / ladoTile).esSolido()) {
+        colision = true;
+    }
+    if (mapa.obtenerCuadro(pixelEsquerdaInferiorX / ladoTile, pixelEsquerdaInferiorY / ladoTile).esSolido()) {
+        colision = true;
+    }
+    if (mapa.obtenerCuadro(pixelDireitaInferiorX / ladoTile, pixelDireitaInferiorY / ladoTile).esSolido()) {
+        colision = true;
+    }
+
+    return colision;
+}
+
 
 	public Sprite obtenerSprite() {
 		return sprite;
