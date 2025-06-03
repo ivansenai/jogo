09a426060def67a0937a52d3383342fd41284b97
Ivan Dutra Goetze <ivangoetze@gmail.com>
2025-06-03 16:19:04 -0300

    Update CuadroVacio.java

--- src/mapa/cuadro/CuadroVacio.java
+++ src/mapa/cuadro/CuadroVacio.java
@@ -4,7 +4,8 @@ import graficos.Sprite;
 
 public class CuadroVacio extends Cuadro {
 
-    public CuadroVacio(Sprite sprite) {
-        super(sprite, true); // Vazio pode ser sólido para representar limites, ou não, dependendo da sua necessidade
+    public CuadroVacio(Sprite sprite, boolean solido) { // Adicionado parâmetro 'solido'
+        super(sprite, solido);
+        // Vazio pode ser sólido para representar limites, ou não, dependendo da sua necessidade
     }
 }
